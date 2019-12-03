fe05a0b682f3678eead06a9c87549aa1396b006f
tdfyoder <tdfyoder@gmail.com>
2019-12-03 13:52:40 -0500

    Update sanger_analysis.py

--- ice/classes/sanger_analysis.py
+++ ice/classes/sanger_analysis.py
@@ -177,8 +177,8 @@ class SangerAnalysis:
     def find_guide_in_ctrl(self, guide_label, guide_seq, ctrl_seq):
         revcomp_guide = reverse_complement(guide_seq)
         found_seq = None
-        print(guide_seq, ctrl_seq)
-        if str.encode(guide_seq) in ctrl_seq:
+        print(type(ctrl_seq))#guide_seq = bytes(guide_seq)
+        if guide_seq in ctrl_seq:
             cut_offset = len(guide_seq) - 3
             orientation = "fwd"
             found_seq = guide_seq
